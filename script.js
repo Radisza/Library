e29c3ecc182c5063c17c6dede3e449fb815718b7
Ela <radisza5@gmail.com>
2024-03-19 14:52:35 +0100

    Add book to library. No input validation.

--- script.js
+++ script.js
@@ -20,27 +20,50 @@ function create_books(num) {
     return books;
 }
 
-function show_all_books(books) {
-    let books_table = document.querySelector('.books');
+function show_books_table(books) {
     for (book of books) {
-        let row = books_table.insertRow(-1);
-
-        let nr = row.insertCell(-1);
-        nr.innerText = books_table.rows.length-1;
-
-        let title = row.insertCell(-1);
-        title.innerText = book.title;
-
-        let author = row.insertCell(-1);
-        author.innerText = book.author;
-
-        let pages = row.insertCell(-1);
-        pages.innerText = book.pages;
-
-        let read = row.insertCell(-1);
-        read.innerText = book.read;
+        add_book_to_table(book)
     }
+}
+function add_book_to_table(book) {
+    let books_table = document.querySelector('.books');
+    let row = books_table.insertRow(-1);
+
+    let nr = row.insertCell(-1);
+    nr.innerText = books_table.rows.length-1;
+
+    let title = row.insertCell(-1);
+    title.innerText = book.title;
+
+    let author = row.insertCell(-1);
+    author.innerText = book.author;
+
+    let pages = row.insertCell(-1);
+    pages.innerText = book.pages;
+
+    let read = row.insertCell(-1);
+    read.innerText = book.read;
+}
+
+function add_book_to_library(book) {
+    books.push(book);
+    add_book_to_table(book);
 }
 
 let books = create_books(10);
-show_all_books(books);
+show_books_table(books);
+
+let book_form = document.querySelector('.new_book');
+book_form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const form_data = new FormData(book_form);
+    console.log(form_data);
+    add_book_to_library (new Book(
+        title = form_data.get("title"),
+        author = form_data.get("author"),
+        pages = form_data.get("pages"),
+        read = form_data.get("read") == "true" ? true : false,
+    ))
+
+    book_form.reset();
+})
